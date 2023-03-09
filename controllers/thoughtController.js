b7e895d79364f7b41f013905782c6ab6daa7f70a
Wir3s <aandrebess@gmail.com>
2023-03-08 21:31:43 -0600

    More user routes working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const Thought = require("../models/Thought");
+const { Thought, User } = require("../models");
 
 module.exports = {
   // Find all thoughts
@@ -23,7 +23,23 @@ module.exports = {
   // create a thought
   createThought(req, res) {
     Thought.create(req.body)
-      .then((dbThoughtData) => res.json(dbThoughtData))
-      .catch((err) => res.status(500).json(err));
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        )
+      })
+      .then((user) => 
+      !user
+        ? res
+             .status(404)
+             .json({ message: `Thought created but no user found with that ID`})
+            : res.json(`Created the thought!`)
+            )
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);
+            })
   },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -27,38 +27,63 @@ module.exports = {
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .select("-__v")
-      // .populate("thought")
+      // .populate("thoughts")
       .then((user) =>
         !user
           ? res.status(404).json({ message: "No such user exists" })
-          : res.json(user)
+          : res.json({user})
       )
       .catch((err) => res.status(500).json(err));
   },
 
   // Find one user, with Async
-    // getSingleUser(req, res) {
-    //   User.findOne({ _id: req.params.userId })
-    //   .select("-__v")
-    //   .then(async (user) =>
-    //    !user
-    //      ? res.status(404).json({ message: 'No user found with that ID' })
-    //      :res.json({
-    //       user,
-    //       // thoughts,
-    //      })
-    //   )
-    //   .catch((err) => {
-    //     console.log(err);
-    //     return res.status(500).json(err);
-    //   });
-    // },
+  // getSingleUser(req, res) {
+  //   User.findOne({ _id: req.params.userId })
+  //   .select("-__v")
+  //   .then(async (user) =>
+  //    !user
+  //      ? res.status(404).json({ message: 'No user found with that ID' })
+  //      :res.json({
+  //       user,
+  //       // thoughts,
+  //      })
+  //   )
+  //   .catch((err) => {
+  //     console.log(err);
+  //     return res.status(500).json(err);
+  //   });
+  // },
   // create a new user
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
-};
 
-// delete a user
+  // delete a user
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID!" })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: "User and thoughts deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
