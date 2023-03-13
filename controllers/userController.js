8f0a0d5ff7ebf33a58c537d0a1e9bcb1deb43254
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-03-13 06:26:09 -0500

    Cleaned comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,16 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-  // Find all users THIS WORKS
+  // Find all users 
   getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
 
-  // Find all users TESTING WTIH ASYNC
-  // getUsers(req, res) {
-  //     User.find()
-  //     .then(async (users) => {
-  //         const userObj = {
-  //             users,
-  //         };
-  //         return res.json(userObj);
-  //     })
-  //     .catch((err) => {
-  //         console.log(err);
-  //         return res.status(500).json(err);
-  //     });
-  // },
 
-  // Find one user - THIS WORKS
+
+  // Find one user 
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .select("-__v")
@@ -37,23 +24,7 @@ module.exports = {
   },
 
 
-  // Find one user, with Async
-  // getSingleUser(req, res) {
-  //   User.findOne({ _id: req.params.userId })
-  //   .select("-__v")
-  //   .then(async (user) =>
-  //    !user
-  //      ? res.status(404).json({ message: 'No user found with that ID' })
-  //      :res.json({
-  //       user,
-  //       // thoughts,
-  //      })
-  //   )
-  //   .catch((err) => {
-  //     console.log(err);
-  //     return res.status(500).json(err);
-  //   });
-  // },
+
   // create a new user
   createUser(req, res) {
     User.create(req.body)
